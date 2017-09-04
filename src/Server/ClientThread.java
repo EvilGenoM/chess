ab40515b749bc75a258d84b17cd82dffcd0f2f51
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-05 01:04:33 +0300

    реализована соединение между игроками

--- src/Server/ClientThread.java
+++ src/Server/ClientThread.java
@@ -10,6 +10,7 @@ import static Server.Server.getUserList;
 public class ClientThread extends Thread {
 
     private Socket socket;
+    private User enemy = null;
 
     public ClientThread(Socket socket){
         this.socket = socket;
@@ -22,21 +23,54 @@ public class ClientThread extends Thread {
 
             String name = in.readUTF();
 
-            getUserList().add(new User(this.socket, in, out, name));
+            User player = new User(this.socket, in, out, name);
+            getUserList().add(player);
 
+            String enemy = in.readUTF();
+
+            if(player.enemy != null){
+                this.enemy = player.enemy;
+                this.enemy.getOutputStream().writeUTF(player.getName()+":" + enemy);
+            }
+
+                while (player.getConnect() == false) {
+                    player.getOutputStream().writeUTF(connect(player, enemy));
+                    if (this.enemy != null) {
+                        player.setConnect();
+                        this.enemy.setConnect();
+                        this.enemy.enemy = player;
+                        break;
+                    }
+                    enemy = in.readUTF();
+                }
+
+            System.out.println("Player" + this.enemy.getName());
             String line = null;
 
             while(true){
                 line = in.readUTF();
                 System.out.println("Получена строка: " + line);
-                for(User user : getUserList()){
-                    user.getOutputStream().writeUTF(user.getName() + ": " + line);
-                }
+                this.enemy.getOutputStream().writeUTF(player.getName() + ": " + line);
             }
 
         } catch (IOException ex){
             System.out.println(ex);
         }
     }
+
+    public String connect(User player, String enemy) throws IOException {
+        String con = " ";
+        for(User user : getUserList()){
+            if(user.getName().equals(enemy)){
+                user.getOutputStream().writeUTF("К вам подсоединился игрок  " + player.getName());
+                this.enemy = user;
+                con = "Успех";
+                break;
+            }
+            con = "Неверное имя. Повторите попытку...";
+        }
+        return con;
+    }
+
 
 }
