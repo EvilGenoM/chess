697d6c6056ad479d5a0239184ed56e992cb6dbc1
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-04 23:04:37 +0300

    добавлена коллекция хранения клиентов, и добавлена многопоточность

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,9 +2,11 @@ package Server;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 public class Server {
     int port = 8080;
+    private static ArrayList<User> userList = new ArrayList<User>();
 
     public static void main(String[] args) {
         Server server = new Server();
@@ -21,13 +23,17 @@ public class Server {
                 while(socket == null){
                     socket = serverSocket.accept();
                 }
-                new ClientThread(socket);
+                new ClientThread(socket).start();
             }
 
         } catch (IOException ex){
             System.out.println(ex);
         }
 
+    }
+
+    public static synchronized ArrayList<User> getUserList(){
+        return userList;
     }
 
 }
