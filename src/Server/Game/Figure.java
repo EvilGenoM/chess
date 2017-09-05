f50bb2aeed54cf73293ae5f5264af1a16a2032b9
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-05 20:40:01 +0300

    объекты всех фигур(без реализации)

--- src/Server/Game/Figure.java
+++ src/Server/Game/Figure.java
@@ -1,4 +1,130 @@
 package Server.Game;
 
-public interface Figure {
+import java.util.*;
+
+public abstract class Figure {
+
+    Map<String, Figure> board;
+
+    Figure(Map<String, Figure> board){
+        this.board = board;
+    }
+
+    abstract void move(String stroke);
+
+    abstract void attack(String stroke);
+}
+
+class King extends Figure{
+
+    King(Map<String, Figure> board) {
+        super(board);
+    }
+
+    @Override
+    public void move(String stroke){
+
+    }
+
+    @Override
+    public void attack(String stroke){
+
+    }
+
+
+}
+
+class Queen extends Figure{
+
+    Queen(Map<String, Figure> board) {
+        super(board);
+    }
+
+    @Override
+    public void move(String stroke){
+
+    }
+
+    @Override
+    public void attack(String stroke){
+
+    }
+
+
+}
+
+class Bishop extends Figure{
+
+    Bishop(Map<String, Figure> board) {
+        super(board);
+    }
+
+    @Override
+    public void move(String stroke){
+
+    }
+
+    @Override
+    public void attack(String stroke){
+
+    }
+
+
+}
+
+class kNight extends Figure{
+
+    kNight(Map<String, Figure> board) {
+        super(board);
+    }
+
+    @Override
+    public void move(String stroke){
+
+    }
+
+    @Override
+    public void attack(String stroke){
+
+    }
+
+}
+
+class Rook extends Figure{
+
+    Rook(Map<String, Figure> board) {
+        super(board);
+    }
+
+    @Override
+    public void move(String stroke){
+
+    }
+
+    @Override
+    public void attack(String stroke){
+
+    }
+
+
+}
+
+
+class Pawn extends Figure{
+
+    Pawn(Map<String, Figure> board) {
+        super(board);
+    }
+
+    @Override
+    public void move(String stroke){
+
+    }
+
+    @Override
+    public void attack(String stroke){
+
+    }
+
+
 }
