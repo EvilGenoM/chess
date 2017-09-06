5a4a3d2d9dbf117d7751daac527b8bf38314830e
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-06 21:18:07 +0300

    добавлены шаг для пешки

--- src/Server/Game/Figure.java
+++ src/Server/Game/Figure.java
@@ -5,25 +5,44 @@ import java.util.*;
 public abstract class Figure {
 
     Map<String, Figure> board;
+    ArrayList<String> strokeFigure = new ArrayList<String>();
+    private String location;
+    public String name = "";
 
     Figure(Map<String, Figure> board){
         this.board = board;
     }
 
-    abstract void move(String stroke);
+    Figure(Map<String, Figure> board, String loc){
+        this.board = board;
+        this.location = loc;
+    }
+
+    abstract boolean move(String stroke);
 
     abstract void attack(String stroke);
+
+    char[] findSymbolNumber(String stroke){
+        char[] element = new char[2];
+        element[0] = stroke.charAt(0);
+        element[1] = stroke.charAt(1);
+
+        return element;
+    }
 }
 
 class King extends Figure{
 
     King(Map<String, Figure> board) {
         super(board);
+        name = "K";
     }
 
     @Override
-    public void move(String stroke){
+    public boolean move(String stroke){
+        boolean result = false;
 
+        return result;
     }
 
     @Override
@@ -38,11 +57,14 @@ class Queen extends Figure{
 
     Queen(Map<String, Figure> board) {
         super(board);
+        name = "Q";
     }
 
     @Override
-    public void move(String stroke){
+    public boolean move(String stroke){
+        boolean result = false;
 
+        return result;
     }
 
     @Override
@@ -57,11 +79,14 @@ class Bishop extends Figure{
 
     Bishop(Map<String, Figure> board) {
         super(board);
+        name = "B";
     }
 
     @Override
-    public void move(String stroke){
+    public boolean move(String stroke){
+        boolean result = false;
 
+        return result;
     }
 
     @Override
@@ -76,11 +101,14 @@ class kNight extends Figure{
 
     kNight(Map<String, Figure> board) {
         super(board);
+        name = "H";
     }
 
     @Override
-    public void move(String stroke){
+    public boolean move(String stroke){
+        boolean result = false;
 
+        return result;
     }
 
     @Override
@@ -94,11 +122,14 @@ class Rook extends Figure{
 
     Rook(Map<String, Figure> board) {
         super(board);
+        name = "R";
     }
 
     @Override
-    public void move(String stroke){
+    public boolean move(String stroke){
+        boolean result = false;
 
+        return result;
     }
 
     @Override
@@ -112,13 +143,71 @@ class Rook extends Figure{
 
 class Pawn extends Figure{
 
-    Pawn(Map<String, Figure> board) {
+    private boolean one;
+    private String location;
+
+    Pawn(Map<String, Figure> board, String loc) {
         super(board);
+        one = true;
+        location = loc;
+        name = "P";
     }
 
     @Override
-    public void move(String stroke){
+    public boolean move(String stroke){
+        boolean result = false;
+        char[] element = findSymbolNumber(location);
+        char[] elementAtack = findSymbolNumber(stroke);
+        int number = Character.getNumericValue(element[1]);
+        /*
+        if((element[0] != 'a')
+                && (elementAtack[0] == element[0]--)
+                && (Character.getNumericValue(elementAtack[1]) == number++)
+                && (board.get(stroke) != null)){
+            attack(stroke);
+            return true;
+        }
 
+        number--;
+
+        if((element[0] != 'h')
+                && (elementAtack[0] == element[0]++)
+                && (Character.getNumericValue(elementAtack[1]) == number++)
+                && (board.get(stroke) != null)){
+            attack(stroke);
+            return true;
+        }
+
+        number--;
+        */
+        if(element[1] != '8'){
+            number++;
+            if(board.get(""+element[0]+number) == null){
+                strokeFigure.add(""+element[0]+number);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        for(String str : strokeFigure){
+            if(str.equals(stroke)){
+                for(Map.Entry entry : board.entrySet()){
+                    String key = (String) entry.getKey();
+                    if(key.equals(stroke)){
+                        entry.setValue(this);
+                    }
+                    if(key.equals(location)){
+                        entry.setValue(null);
+                    }
+                }
+                this.location = str;
+                return true;
+            }
+        }
+
+        return result;
     }
 
     @Override
