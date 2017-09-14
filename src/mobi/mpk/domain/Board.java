8faf5cbf4e3ba120e26ef746a01d8511b05d0201
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-15 00:32:15 +0300

    внесены не значительные изменения

--- src/mobi/mpk/domain/Board.java
+++ src/mobi/mpk/domain/Board.java
@@ -11,11 +11,15 @@ public class Board {
     private List<Figure> blackFigure;
 
     public Board(){
+
         cells = new Cell[8][8];
+
         for(int x = 0; x < 8; x++){
+
             for (int y = 0; y < 8; y++){
                 cells[x][y] = new Cell(x,y);
             }
+
         }
 
     }
