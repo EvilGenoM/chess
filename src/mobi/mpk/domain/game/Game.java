84e092c60b1d6c1cab926ab118b148e797c31174
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:00:18 +0300

    добавлен класс описывающий игру

--- src/mobi/mpk/domain/game/Game.java
+++ src/mobi/mpk/domain/game/Game.java
@@ -1,14 +1,55 @@
-package mobi.mpk.domain;
+package mobi.mpk.domain.game;
+
+import mobi.mpk.domain.*;
+import mobi.mpk.domain.rules.Rules;
+import mobi.mpk.net.User;
+
+import java.util.Date;
+
+public abstract class Game {
+
+    private Date beginTime;
+
+    private Player playerWhite;
+    private Player playerBlack;
+
+    private Board board;
+
+    private Rules rools;
+
+    public Game(Player player1, Player player2, Rules rools){
+
+        this.rools = rools;
+
+        Player[] players = this.rools.identifyWhitePlayer(player1, player2);
+
+        this.playerWhite = players[0];
+
+        this.playerBlack = players[1];
+
+        this.beginTime = new Date();
+
+        this.board = new Board();
 
 
-import javax.xml.crypto.Data;
+    }
 
-public class Game {
+    public abstract String handleStroke(Stroke stroke, User user);
 
-    private Data beginData;
-    private Player player1;
-    private Player player2;
+    protected abstract void initBoard();
 
+    protected Board getBoard(){
+        return board;
+    }
 
+    protected Rules getRools() { return rools; }
+
+    protected Player getPlayerBlack() {
+        return playerBlack;
+    }
+
+    protected Player getPlayerWhite() {
+        return playerWhite;
+    }
 
 }
