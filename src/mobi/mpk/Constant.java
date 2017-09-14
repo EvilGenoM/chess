0d566e8aacb3a5277635c46b1076b42e4b43366e
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:10:26 +0300

    добавлен класс констант

--- src/mobi/mpk/Constant.java
+++ src/mobi/mpk/Constant.java
@@ -1,4 +1,12 @@
 package mobi.mpk;
 
 public class Constant {
+
+    public final static String REPLY_HELP = "join Name - отправить приглашение в игру" +
+                                            "\nplayers - вывести список игроков" +
+                                            "\nclose - выйти из приложения";
+    public final static String LOG_COMMAND = "Выполнена команда: ";
+
+    public final static String ERROR_COMMAND = "Неизвестная команда...";
+
 }
