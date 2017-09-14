8bb2f89c561d355c63b9a6270b5686da0733f322
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-15 00:41:21 +0300

    добавлен класс отвечающий за реализацию правил хода фигур при классической игре

--- src/mobi/mpk/domain/rules/csfigure/ClassicRulesStrokesFigure.java
+++ src/mobi/mpk/domain/rules/csfigure/ClassicRulesStrokesFigure.java
@@ -1,4 +1,48 @@
 package mobi.mpk.domain.rules.csfigure;
 
-public interface ClassicRulesStrokesFigure {
+import mobi.mpk.domain.Board;
+import mobi.mpk.domain.Cell;
+import mobi.mpk.domain.figure.Figure;
+import mobi.mpk.domain.rules.RulesStrokesFigure;
+
+public class ClassicRulesStrokesFigure implements RulesStrokesFigure {
+
+    @Override
+    public boolean strokeFigure(Cell from, Cell to, Board board){
+
+        Figure figure;
+
+        if(from.getFigure() != null){
+            figure = from.getFigure();
+        } else {
+            return false;
+        }
+
+        switch (figure.getClass().getSimpleName()){
+            case "King":
+                System.out.println("1");
+                return strokesKing(from, to, board);
+            case "Queen":
+                System.out.println("2");
+                return strokeQueen(from, to, board);
+            case "Bishop":
+                System.out.println("3");
+                return strokeBishop(from, to, board);
+            case "kNight":
+                System.out.println("4");
+                return strokesKNight(from, to);
+            case "Rook":
+                System.out.println("5");
+                return strokesRook(from, to, board);
+            case "Pawn":
+                System.out.println("6");
+                return strokesPawn(from, to, board);
+            default:
+                return false;
+
+        }
+
+    }
+
+
 }
