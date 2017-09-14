6796b9e08561f02f42b7323a41c7790d07471010
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:13:31 +0300

    добавлен интерфейс определяющий конект

--- src/mobi/mpk/controller/Connect.java
+++ src/mobi/mpk/controller/Connect.java
@@ -1,4 +1,11 @@
 package mobi.mpk.controller;
 
 public interface Connect {
+
+    Controller getPlayer1Controller();
+
+    Controller getPlayer2Controller();
+
+    Reply execute(Request request, Controller control);
+
 }
