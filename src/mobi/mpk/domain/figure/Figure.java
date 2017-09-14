34455652a35478260c42bf7b23662a0a36b9c178
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:02:46 +0300

    добавлен абстрактный класс описывающий фигуру

--- src/mobi/mpk/domain/figure/Figure.java
+++ src/mobi/mpk/domain/figure/Figure.java
@@ -1,4 +1,16 @@
 package mobi.mpk.domain.figure;
 
-public class Figure {
+import mobi.mpk.domain.Color;
+
+public abstract class Figure {
+
+    private Color color;
+
+    public Figure(Color color){
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
