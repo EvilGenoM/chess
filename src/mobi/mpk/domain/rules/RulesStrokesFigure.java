d08ef4f20db1eeec3f56dbb55f061d04a860214a
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-15 00:37:10 +0300

    добавлен интерфейс отвечающий за правила движения фигур

--- src/mobi/mpk/domain/rules/RulesStrokesFigure.java
+++ src/mobi/mpk/domain/rules/RulesStrokesFigure.java
@@ -1,4 +1,8 @@
 package mobi.mpk.domain.rules;
 
+import mobi.mpk.domain.Board;
+import mobi.mpk.domain.Cell;
+
 public interface RulesStrokesFigure {
+    boolean strokeFigure(Cell from, Cell to, Board board);
 }
