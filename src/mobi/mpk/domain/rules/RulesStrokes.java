1f992aa34e1290f7f9b84d1ca2f904dbd6d587c2
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:06:19 +0300

    добавлен интерфейс определяющий правила хода фигуры

--- src/mobi/mpk/domain/rules/RulesStrokes.java
+++ src/mobi/mpk/domain/rules/RulesStrokes.java
@@ -1,4 +1,10 @@
 package mobi.mpk.domain.rules;
 
+import mobi.mpk.domain.Board;
+import mobi.mpk.domain.Cell;
+
 public interface RulesStrokes {
+
+    boolean strokeFigure(Cell from, Cell to, Board board);
+
 }
