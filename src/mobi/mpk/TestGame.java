c8fa1b95b97e08c89ce41d2cde607d4a6a3b099e
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:10:48 +0300

    добавлен тестовый класс

--- src/mobi/mpk/TestGame.java
+++ src/mobi/mpk/TestGame.java
@@ -3,21 +3,32 @@ package mobi.mpk;
 import junit.framework.TestCase;
 import mobi.mpk.domain.Cell;
 import mobi.mpk.domain.Color;
+import mobi.mpk.domain.Player;
+import mobi.mpk.domain.Stroke;
 import mobi.mpk.domain.figure.Pawn;
 import mobi.mpk.domain.figure.kNight;
+import mobi.mpk.domain.game.ClassicChessGame;
+import mobi.mpk.domain.game.Game;
 import mobi.mpk.domain.rules.ClassicRulesStrokes;
 import mobi.mpk.domain.rules.RulesStrokes;
+import mobi.mpk.net.User;
 import org.junit.*;
 
 import static org.junit.Assert.*;
 
-public class TestRulesStrokes {
+public class TestGame {
 
     private RulesStrokes rulesStrokes;
+    private Game game;
+    private Player player1;
+    private Player player2;
 
     @Before
     public void init(){
+        player1 = new Player();
+        player2 = new Player();
         rulesStrokes = new ClassicRulesStrokes();
+        game = new ClassicChessGame(player1, player2);
     }
 
     @After
@@ -26,7 +37,7 @@ public class TestRulesStrokes {
     }
 
     @org.junit.Test
-    public void strokeKNight(){
+    public void strokeFigure(){
 
         Cell[] to = new Cell[8];
         to[0] = new Cell('d',2, null);
@@ -36,7 +47,7 @@ public class TestRulesStrokes {
         to[4] = new Cell('g',3, null);
         to[5] = new Cell('c',5, null);
         to[6] = new Cell('d',6, null);
-        to[7] = new Cell('f',6, new kNight(Color.white));
+        to[7] = new Cell('f',6, new kNight(Color.black));
 
         for(int i = 0; i<8; i++){
 
@@ -46,6 +57,11 @@ public class TestRulesStrokes {
 
         }
 
+
+    }
+
+    @org.junit.Test
+    public void movePlayer(){
 
     }
 
