e880b0a95c1d4b001404672a42dcd2ef3254a950
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 20:12:10 +0300

    добавлен класс описывающий юзера

--- src/mobi/mpk/net/User.java
+++ src/mobi/mpk/net/User.java
@@ -1,84 +1,44 @@
-package mobi.mpk.server;
+package mobi.mpk.net;
 
-import mobi.mpk.server.Game.ChessBoard;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.Socket;
-
-import static mobi.mpk.server.Server.getUserList;
+import static mobi.mpk.net.Server.getUserList;
 
 public class User {
-    private Socket socket;
-    private DataInputStream inputStream;
-    private DataOutputStream outputStream;
-    private String name;
-    private boolean connect = false;
-    User enemy = null;
-    private boolean stroke = false;
-    private boolean con = true;
-    private ChessBoard board;
-    private boolean white;
 
-    public User(Socket socket){
-        this.socket = socket;
+    private String name = null;
+    private boolean connect;
+
+    public String getName(){
+        return this.name;
     }
 
-    public User(Socket socket, DataInputStream inputStream, DataOutputStream outputStream, String name){
-        this.socket = socket;
-        this.inputStream = inputStream;
-        this.outputStream = outputStream;
-        this.name = this.createName(name);
-        this.connect = false;
-    }
-
-    public Socket getSocket(){
-        return this.socket;
-    }
-
-    public DataInputStream getInputStream(){
-        return this.inputStream;
-    }
-
-    public DataOutputStream getOutputStream() {
-        return this.outputStream;
-    }
-
-    public  String getName(){ return this.name; }
-
-    public boolean getConnect(){ return  this.connect; }
-
-    public void setConnect(boolean connect){ this.connect = connect; }
-
-    synchronized public void setCon(boolean con){ this.con = con;}
-
-    synchronized public boolean getCon(){ return this.con;}
-
-    synchronized public void setStroke(boolean stroke){ this.stroke = stroke;}
-
-    synchronized public boolean getStroke(){ return this.stroke;}
-
-    synchronized public void setChessBoard(ChessBoard board){ this.board = board;}
-
-    synchronized public ChessBoard getChessBoard(){ return this.board;}
-
-    synchronized public void setWhite(boolean white){ this.white = white;}
-
-    synchronized public boolean getWhite(){ return this.white;}
-
-    private String createName(String name){
-
-        String fakeName = name;
-        int i = 1;
+    public String setName(String name){
 
         for(User user : getUserList()){
-            if(user.name.equals(fakeName)){
-                fakeName = name + i;
-                i++;
+
+            if(user.getName().equals(name)){
+                return "Имя уже существует";
             }
+
         }
 
-        return fakeName;
+        this.name = name;
 
+        return "Ваше имя: " + this.name;
+
+    }
+
+    public boolean equals(User user){
+        if(this.name.equals(user.getName())){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean getConnect(){ return this.connect; }
+
+    public void setConnect(boolean connect) {
+        this.connect = connect;
     }
 }
