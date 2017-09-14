c736a70ccca60339b7527181bd2b6844c9280e35
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 19:57:26 +0300

    класс определяющий ход

--- src/mobi/mpk/domain/Stroke.java
+++ src/mobi/mpk/domain/Stroke.java
@@ -1,4 +1,46 @@
 package mobi.mpk.domain;
 
+import java.util.Date;
+
 public class Stroke {
+
+
+    private Cell from;
+    private Cell to;
+
+    private Date time;
+
+
+    public Stroke(String stroke){
+
+        String coordCell[] = stroke.split(" ");
+
+        if(coordCell[0] != null && coordCell[1] != null){
+            this.from = createCell(coordCell[0]);
+            this.to = createCell(coordCell[1]);
+        }
+
+        this.time = new Date();
+
+    }
+
+    public Cell getFrom() {
+        return from;
+    }
+
+    public Cell getTo() {
+        return to;
+    }
+
+    private Cell createCell(String coordCell){
+
+
+        char xCoordCell = coordCell.charAt(0);
+        int yCoordCell = Character.getNumericValue(coordCell.charAt(1));
+
+        return new Cell(xCoordCell, yCoordCell);
+
+    }
+
+
 }
