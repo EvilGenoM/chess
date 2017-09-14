a03dc0dd1e2bac1d3d1b2cb280a8da6ce32b66db
Evgen Mezentsev <evmezentsev@gmail.com>
2017-09-14 19:58:34 +0300

    добавлен класс описывающий игрока

--- src/mobi/mpk/domain/Player.java
+++ src/mobi/mpk/domain/Player.java
@@ -1,4 +1,62 @@
 package mobi.mpk.domain;
 
+import mobi.mpk.domain.rules.Rules;
+import mobi.mpk.net.User;
+
 public class Player {
+
+    private User user;
+
+    public Player(User user){
+        this.user = user;
+    }
+
+    public Player(){
+
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+
+    public String move(Stroke stroke, Board board, Rules rules){
+
+        Cell[][] cells = board.getMassiveCell();
+        Cell from = null;
+        Cell to = null;
+
+        for(int x = 0; x<8; x++){
+
+            for (int y = 0; y<8; y++){
+
+                if(cells[x][y].equals(stroke.getFrom())){
+
+                    from = cells[x][y];
+
+                } else if(cells[x][y].equals(stroke.getTo())){
+
+                    to = cells[x][y];
+
+                }
+
+            }
+
+        }
+
+        if(from != null && to != null){
+
+            boolean doStroke = rules.move(from, to, board);
+
+            if(doStroke){
+                return "Ход успешно сделан";
+            } else {
+                return "Ошибка хода";
+            }
+
+        }
+
+        return "Ошибка хода";
+
+    }
+
 }
